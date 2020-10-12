5e4193522a3dba33515ad4ab90cdd217a0ab92ac
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-12 19:59:10 -0400

    calls default_order function from MagicCardsController

--- app/controllers/magic_cards_controller.rb
+++ app/controllers/magic_cards_controller.rb
@@ -1,6 +1,6 @@
 class MagicCardsController < ApplicationController
     def index
         magic_cards = MagicCard.all
-        render json:magic_cards
+        render json:magic_cards.default_order
     end
 end
