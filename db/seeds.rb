a82f862785451dada33e68546cd52f249ca5d32c
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-12 20:06:28 -0400

    creates access_token"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ENV["api_private_key"]
+ENV["api_public_key"]
+payload = "grant_type=client_credentials&client_id="+ENV["api_public_key"]+"&client_secret="+ENV["api_private_key"]
+
+token = RestClient.post("https://api.tcgplayer.com/token", payload  ,headers={"content-type": "application/x-www-form-urlencoded"})
+
+token = JSON.parse(token)
+@access_token = token['access_token']
