132d5efae6b506f75baa89448281f5e921e5de65
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-11 00:58:12 -0500

    adds self.alphabetical and to_param functions to magic_card

--- app/models/magic_card.rb
+++ app/models/magic_card.rb
@@ -2,4 +2,11 @@ class MagicCard < ApplicationRecord
     def self.default_order
         order('id ASC')
     end
+
+    def self.alphabetical
+        order(:name)
+    end
+    # def to_param
+    #     "#{id}-#{name}"
+    # end
 end
