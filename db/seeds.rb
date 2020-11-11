9cecb9fb14aec35a3f3f4c8af7eb6e7b781145dd
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-11 05:41:34 -0500

    updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -140,7 +140,7 @@ token = JSON.parse(token)
 #     # puts 'foil => ', c['foil']
 # end
 
-MagicCard.default_order.all.slice(0, 25000).each do |c|
+MagicCard.default_order.all.slice(25000, MagicCard.default_order.all.length).each do |c|
     puts ' id => ' + c['id'].to_s
     puts 'group id => ' + c['group_id'].to_s
 
