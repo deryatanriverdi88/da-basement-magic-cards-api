cc2b675fba8280d60abf6ba9df9ff264d569cecc
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-11 00:58:27 -0500

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :magic_cards
+
+  get '/last_ten', to: 'magic_cards#last_ten', as: 'last_ten'
+  get '/token', to: 'magic_cards#token', as: 'token'
+  get '/card', to: 'magic_cards#card', as: 'card'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
