d4c5fffad4bb88c1005f2c8327768fc2d45f77d5
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-19 01:34:39 -0500

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_012033) do
+ActiveRecord::Schema.define(version: 2020_11_19_063334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_012033) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "icon"
+    t.string "color"
   end
 
 end
